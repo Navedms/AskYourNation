4005d84f6078ebde5b5af4de5c4833c47873f0a0
Ohad Nave <ohadnave@Ohads-MacBook-Pro.local>
2023-10-04 12:58:56 +0300

    fix bug sendEmail on cyclic

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,38 +1,51 @@
-import nodemailer from 'nodemailer';
+import { log } from "console";
+import nodemailer from "nodemailer";
 
 interface MailSettings {
-  host: string;
-  port: any;
-  secure: boolean;
-  auth: {
-    user: string;
-    pass: string;
-  };
+	host: string;
+	port: any;
+	secure: boolean;
+	auth: {
+		user: string;
+		pass: string;
+	};
 }
 
 const sendEmail = async (email: string, subject: string, text: string) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    } as MailSettings);
+	try {
+		const transporter = nodemailer.createTransport({
+			host: process.env.EMAIL_HOST,
+			port: process.env.EMAIL_PORT,
+			secure: true,
+			auth: {
+				user: process.env.EMAIL_USER,
+				pass: process.env.EMAIL_PASS,
+			},
+		} as MailSettings);
 
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: subject,
-      html: text,
-    });
-    console.log('email sent sucessfully');
-  } catch (error) {
-    console.log('email not sent');
-    console.log(error);
-  }
+		const result = await new Promise((resolve, reject) => {
+			transporter.sendMail(
+				{
+					from: process.env.EMAIL_USER,
+					to: email,
+					subject: subject,
+					html: text,
+				},
+				function (error, response) {
+					if (error) {
+						console.log(error);
+						reject(error);
+					} else {
+						resolve("email sent sucessfully");
+					}
+				}
+			);
+		});
+		console.log(result);
+	} catch (error) {
+		console.log("email not sent");
+		console.log(error);
+	}
 };
 
 export default sendEmail;
