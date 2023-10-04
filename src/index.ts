657e390170a21d86d98d18b01651c8c94d5653a3
Ohad Nave <ohadnave@Ohads-MacBook-Pro.local>
2023-10-04 13:28:59 +0300

    fix bug sendEmail on cyclic

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,6 @@ const startServer = () => {
 	});
 
 	router.listen(config.server.port, () =>
-		console.log(`Server listening on port ${config.server.port} mail host: ${process.env.EMAIL_HOST}
- port: ${process.env.EMAIL_PORT} user: ${process.env.EMAIL_USER} pass: ${process.env.EMAIL_PASS}`)
+		console.log(`Server listening on port ${config.server.port}`)
 	);
 };
